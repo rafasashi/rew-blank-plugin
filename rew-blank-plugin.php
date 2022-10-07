bf230e0cbfd08d57d36f2516bb0a741229d6c5a4
Rafasashi\Raphael <raphael.dartigues@gmail.com>
2022-10-07 12:18:11 +0545

    first commit

---
+++ rew-blank-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Blank Plugin
+ * Plugin URI: https://wordpress.recuweb.com/installer/blank-plugin/
+ * Description: A blank WP Plugin
+ * Version: 1.0.0
+ * Author: Rafasashi
+ * Author URI: https://wordpress.recuweb.com
+ * License: GPL3
+ */
+
+// Start writing code after this line!
